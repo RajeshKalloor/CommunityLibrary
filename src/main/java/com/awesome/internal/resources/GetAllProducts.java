849d800ee744094dd053ef3f258379371e676b52
Rajesh Kalloor <rajesh.kalloor@flipkart.com>
2015-06-05 23:46:13 +0530

    creating new project - Community Library

---
+++ src/main/java/com/awesome/internal/resources/GetAllProducts.java
@@ -0,0 +1,27 @@
+package com.awesome.internal.resources;
+
+import com.awesome.flipbid.config.MessagesConfiguration;
+import com.awesome.internal.impl.ProductImpl;
+import com.awesome.structures.Product;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sankeerth.reddy on 05/06/15.
+ */
+@Path(value = "/getAllProducts")
+@Produces(MediaType.APPLICATION_JSON)
+public class GetAllProducts {
+
+        public GetAllProducts() {}
+
+        @GET
+        public ArrayList<Product> getAllProducts() {
+            return new ProductImpl().getAllProducts();
+        }
+}
